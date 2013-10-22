de52d9160d938f7b2dc279fd907ec45b2b947ff4
George Lee <keoki.lee@gmail.com>
2013-10-22 14:01:06 -0700

    Finished step 2.

--- lib/sparsify.rb
+++ lib/sparsify.rb
@@ -7,18 +7,31 @@ module Sparsify
 
     result = {}
     source.each do |k, v|
-      process(result, k, v)
+      sparsify(result, k, v)
     end
 
     result
   end
 
-  def self.process(result, key, value)
+  def self.unsparse(source)
+    raise ArgumentError.new("ERROR: wrong number of arguments") if source.nil?
+
+    result = {}
+    source.each do |k, v|
+      unsparsify(result, k, v)
+    end
+
+    result
+  end
+
+  private
+
+  def self.sparsify(result, key, value)
     case value
     when Hash
       tmp = {}
       value.each do |k, v|
-        process(tmp, k, v)
+        sparsify(tmp, k, v)
       end
 
       tmp.each do |k, v|
@@ -26,6 +39,29 @@ module Sparsify
       end
     else
       result[key] = value
+    end
+  end
+
+  def self.unsparsify(result, key, value, level=0)
+    first, rest = key.split(".", 2)
+
+    if rest.nil?
+      result[first] = value
+    else
+      tmp = {}
+      unsparsify(tmp, rest, value, level += 1)
+
+      if result.has_key?(first)
+        tmp.each do |k, v|
+          if result[first].has_key?(k)
+            result[first][k] = result[first][k].merge(v)
+          else
+            result[first][k] = v
+          end
+        end
+      else
+        result[first] = tmp
+      end
     end
   end
 end
